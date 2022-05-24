a57f682dcac2af369055489f609b8c16ae6fa715
Klaas Schoute <klaas_schoute@hotmail.com>
2022-05-24 03:31:10 +0200

    Update to work with version v2.1.0 (#49)
    * Update to work with latest version of package
    
    * Add collapse feature

--- cities/eindhoven.py
+++ cities/eindhoven.py
@@ -1,30 +1,39 @@
-import uuid, parking_eindhoven, datetime, aiohttp
-
+import uuid, datetime
+from parking_eindhoven import ParkingEindhoven
 from database import connection, cursor
 
 city = "Eindhoven"
 
-async def async_get_locations(number, type):
-    """Get parking data from API."""
+async def async_get_locations(number):
+    """Get parking data from API.
 
-    async with aiohttp.ClientSession() as client:
-        return await parking_eindhoven.get_locations(number, type, client)
+    Args:
+        number: The number of parking lots to get.
+    """
+    async with ParkingEindhoven(parking_type=3) as client:
+        locations = await client.locations(rows=number)
+        return locations
 
 def upload(data_set):
-    """Upload the data_set to the database."""
+    """Upload the data_set to the database.
 
+    Args:
+        data_set: The data_set to upload.
+    """
+    count: int
     try:
-        for item in data_set:
-            id = uuid.uuid4().hex[:8]
+        for index, item in enumerate(data_set, 1):
+            count = index
+            # Define unique id
+            location_id = uuid.uuid4().hex[:8]
+
             sql = """INSERT INTO `parking_cities` (`id`, `city`, `street`, `orientation`, `number`, `longitude`, `latitude`, `visibility`, `created_at`, `updated_at`)
                      VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
-            val = (id, str(city), str(item.street), None, item.number, float(item.longitude), float(item.latitude), bool(True), (datetime.datetime.now()), (datetime.datetime.now()))
+            val = (location_id, str(city), str(item.street), None, item.number, float(item.longitude), float(item.latitude), bool(True), (datetime.datetime.now()), (datetime.datetime.now()))
             cursor.execute(sql, val)
         connection.commit()
     except Exception as e:
         print(f'MySQL error: {e}')
     finally:
+        print(f"{count} - Parkeerplaatsen gevonden")
         print(f'{city} - KLAAR met updaten van database')
-
-def update():
-    return
