158bfd0f037bb7afe6499d9d0e6b2e51b53d8739
Klaas Schoute <hello@student-techlife.com>
2022-05-31 02:51:56 +0200

    Add country and province ID to cities (#55)
    * Add country and province ID to cities
    
    * Added to much values to 2 cities

--- cities/eindhoven.py
+++ cities/eindhoven.py
@@ -2,7 +2,7 @@ import uuid, datetime
 from parking_eindhoven import ParkingEindhoven
 from database import connection, cursor
 
-city = "Eindhoven"
+municipality = "Eindhoven"
 
 async def async_get_locations(number):
     """Get parking data from API.
@@ -27,13 +27,13 @@ def upload(data_set):
             # Define unique id
             location_id = uuid.uuid4().hex[:8]
 
-            sql = """INSERT INTO `parking_cities` (`id`, `city`, `street`, `orientation`, `number`, `longitude`, `latitude`, `visibility`, `created_at`, `updated_at`)
-                     VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
-            val = (location_id, str(city), str(item.street), None, item.number, float(item.longitude), float(item.latitude), bool(True), (datetime.datetime.now()), (datetime.datetime.now()))
+            sql = """INSERT INTO `parking_cities` (`id`, `country_id`, `province_id`, `municipality`, `street`, `orientation`, `number`, `longitude`, `latitude`, `visibility`, `created_at`, `updated_at`)
+                     VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+            val = (location_id, int(157), int(11), str(municipality), str(item.street), None, item.number, float(item.longitude), float(item.latitude), bool(True), (datetime.datetime.now()), (datetime.datetime.now()))
             cursor.execute(sql, val)
         connection.commit()
     except Exception as e:
         print(f'MySQL error: {e}')
     finally:
         print(f"Aantal parkeerplaatsen gevonden: {count}")
-        print(f'{city} - KLAAR met updaten van database')
+        print(f'{municipality} - KLAAR met updaten van database')
