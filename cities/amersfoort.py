186e9e182f57cdde3470beb7aefa0bdf81a602a8
Klaas Schoute <hello@student-techlife.com>
2022-07-14 13:32:03 +0200

    Generate unique_id based on location cords

--- cities/amersfoort.py
+++ cities/amersfoort.py
@@ -29,6 +29,37 @@ def centroid(vertexes):
     _y = sum(_y_list) / _len
     return(_y, _x)
 
+def get_unique_number(lat, lon):
+    """Generate a unique number for the location.
+    
+    Args:
+        lat (float): The latitude of the location.
+        lon (float): The longitude of the location.
+
+    Returns:
+        int: The unique number.
+    """
+    try:
+        lat_double = None
+        lon_double = None
+        if isinstance(lat, str):
+            lat_double = float(lat)
+        else:
+            lat_double = lat
+        if isinstance(lon, str):
+            lon_double = float(lon)
+        else:
+            lon_double = lon
+
+        lat_int = int((lat_double * 1e7))
+        lon_int = int((lon_double * 1e7))
+        val = abs((lat_int << 16 & 0xffff0000) | (lon_int & 0x0000ffff))
+        val = val % 2147483647
+        return val
+    except Exception as e:
+        print("marking OD_LOC_ID as -1 getting exception inside get_unique_number function")
+        print("Exception while generating od loc id")
+
 
 async def async_get_locations():
     """Get the data from the CKAN API endpoint."""
@@ -58,23 +89,22 @@ def upload(data_set):
             # Get the coordinates of the parking lot with centroid
             latitude, longitude = centroid(item["geometry"]["coordinates"])
             # Define unique id
-            location_id = f"{cbs_code}-{item['id'].split('.')[1]}"
+            location_id = f"{cbs_code}-{get_unique_number(latitude, longitude)}"
 
             item = item["properties"]
             # Make the sql query
-            sql = """INSERT INTO `parking_cities` (`id`, `country_id`, `province_id`, `municipality`, `street`, `orientation`, `number`, `longitude`, `latitude`, `visibility`, `created_at`, `updated_at`)
-                     VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) ON DUPLICATE KEY
+            sql = """INSERT INTO `parking_cities` (id, country_id, province_id, municipality, street, number, longitude, latitude, visibility, created_at, updated_at)
+                     VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) ON DUPLICATE KEY
                      UPDATE id=values(id),
                             country_id=values(country_id),
                             province_id=values(province_id),
                             municipality=values(municipality),
                             street=values(street),
-                            orientation=values(orientation),
                             number=values(number),
                             longitude=values(longitude),
                             latitude=values(latitude),
                             updated_at=values(updated_at)"""
-            val = (location_id, int(157), int(7), str(municipality), str(item["STRAATNAAM"]), None, int(item["AANTAL_PLAATSEN"]),
+            val = (location_id, int(157), int(7), str(municipality), str(item["STRAATNAAM"]), int(item["AANTAL_PLAATSEN"]),
                    float(longitude), float(latitude), bool(True), (datetime.datetime.now(tz=pytz.timezone('Europe/Amsterdam'))),
                    (datetime.datetime.now(tz=pytz.timezone('Europe/Amsterdam'))))
             cursor.execute(sql, val)
