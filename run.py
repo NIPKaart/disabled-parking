9a6974c1bbec7a128219ffa91f6cbb9c3b07b4a5
Klaas Schoute <hello@student-techlife.com>
2022-08-03 01:41:54 +0200

    Small improvement on run code

--- run.py
+++ run.py
@@ -23,53 +23,55 @@ if __name__ == "__main__":
     env_path = Path(".") / ".env"
     load_dotenv(dotenv_path=env_path)
     city: str = os.getenv("CITY")
+    city = city.lower()
 
     print("--- Start program ---")
-    if city.lower() == "amsterdam":
+    if city == "amsterdam":
         # Amsterdam
         data_set = asyncio.run(amsterdam.async_get_locations())
         if bool(data_set):
             print(f"Data opgehaald van: {city}")
             # database.truncate("Amsterdam")
             amsterdam.upload(data_set)
-    elif city.lower() == "arnhem":
+    elif city == "arnhem":
         # Arnhem
         arnhem.download()
         # database.truncate("Arnhem")
         arnhem.upload()
-    elif city.lower() == "amersfoort":
+    elif city == "amersfoort":
         # Amersfoort
         data_set = asyncio.run(amersfoort.async_get_locations())
         if bool(data_set):
             print(f"Data opgehaald van: {city}")
             # database.truncate("Amersfoort")
             amersfoort.upload(data_set)
-    elif city.lower() == "eindhoven":
+    elif city == "eindhoven":
         # Eindhoven
         data_set = asyncio.run(eindhoven.async_get_locations(200))
         if bool(data_set):
             print(f"Data opgehaald van: {city}")
             # database.truncate("Eindhoven")
             eindhoven.upload(data_set)
-    elif city.lower() == "denhaag":
+    elif city == "denhaag":
         # Den Haag
         data_set = asyncio.run(den_haag.async_get_locations(limit=300))
         if bool(data_set):
             print(f"Data opgehaald van: {city}")
             # database.truncate("Den Haag")
             den_haag.upload(data_set)
-    elif city.lower() == "hamburg":
+    elif city == "hamburg":
         # Hamburg
         data_set = asyncio.run(hamburg.async_get_locations())
         if bool(data_set):
             print(f"Data opgehaald van: {city}")
             # database.truncate("Hamburg")
             hamburg.upload(data_set)
-    elif city.lower() == "groningen":
+    elif city == "groningen":
         # Groningen
         groningen.download()
+        # database.truncate("Groningen")
         groningen.upload()
-    elif city.lower() == "zoetermeer":
+    elif city == "zoetermeer":
         # Zoetermeer
         zoetermeer.download()
         # database.truncate("Zoetermeer")
